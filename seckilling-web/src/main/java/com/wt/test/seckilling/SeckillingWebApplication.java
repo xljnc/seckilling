ece4cff6aab27d24ada063c200789b829207191b
qiyu <jsj0804wt@126.com>
2019-12-19 11:27:15 +0800

    创建模块 by qiyu 2019-Dec-19

---
+++ seckilling-web/src/main/java/com/wt/test/seckilling/SeckillingWebApplication.java
@@ -0,0 +1,13 @@
+package com.wt.test.seckilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillingWebApplication.class, args);
+    }
+
+}
---
+++ seckilling-web/src/test/java/com/wt/test/seckilling/SeckillingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.test.seckilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
