196172a442bb5a33ce73743cf0ef8fe24a243182
qiyu <jsj0804wt@126.com>
2019-12-19 11:51:57 +0800

    创建模块 by qiyu 2019-Dec-19

--- seckilling-web/src/main/java/com/wt/seckilling/SeckillingWebApplication.java
+++ seckilling-web/src/main/java/com/wt/seckilling/SeckillingWebApplication.java
@@ -1,4 +1,4 @@
-package com.wt.test.seckilling;
+package com.wt.seckilling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- seckilling-web/src/test/java/com/wt/seckilling/SeckillingWebApplicationTests.java
+++ seckilling-web/src/test/java/com/wt/seckilling/SeckillingWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wt.test.seckilling;
+package com.wt.seckilling;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
